18b3a6483e294f461220e3ef9d95048122ab7925
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-24 18:47:45 +0200

    add newStory endpoint

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from "express"
 import pinoHTTP from 'pino-http'
 import logger from "./logger.mjs";
+import { newStory } from "./postCalls.mjs";
 import  cors  from "cors";
 import { selectSubmissions, selectStories, selectPublishers, selectGenres } from "./selectCalls.mjs";
 
@@ -29,17 +30,27 @@ app.get('/api/stories', async (req,res) => {
   res.send(result)
 })
 app.get('/api/publishers', async (req,res) => {
-  logger.info("submissions request received!")
+  logger.info("publishers request received!")
   res.statusCode = 200
   const result = await selectPublishers()
   res.send(result)
 })
 app.get('/api/genres', async (req,res) => {
-  logger.info("submissions request received!")
+  logger.info("genres request received!")
   res.statusCode = 200
   const result = await selectGenres()
   res.send(result)
 })
+
+app.post('/api/stories', async (req,res) => {
+  logger.info("add story request received!")
+  res.statusCode = 200
+  const data = req.body
+  const result = await newStory(data)
+})
+
+
+
 
 app.listen(port, (err)=>{
     if (err) logger.error(err);
